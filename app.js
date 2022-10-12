2ebe42737fb19471cfb2d98eae89b6a311a3dd1c
Joshua <josh_perkins-@hotmail.com>
2022-10-13 00:02:22 +1100

    add ons

--- app.js
+++ app.js
@@ -88,7 +88,68 @@
 // console.log(convertcelciusToFahrenheit (30)) 
 
 
-const convertcelciusToFahrenheit = (celsius) => {
-    return celsius * 1.8 + 32
-}
-console.log(convertcelciusToFahrenheit (0)) 
+// const convertcelciusToFahrenheit = (celsius) => {
+//     return celsius * 1.8 + 32
+// }
+// console.log(convertcelciusToFahrenheit (0)) 
+
+
+// let item1 = 20
+// let item2 = 30
+// let item3 = 40
+// let item4 = 50
+// let item5 = 100
+
+
+// let arr = [20, 30, 40, 50, 100]
+
+
+// first element of array
+// console.log(arr[0])
+
+
+// // last Element of array 
+// console.log(arr [arr.length - 1])
+
+// // add element onto the end of array:
+// arr.push(200)
+
+
+// let newArr = arr.filter((element) => {
+//     console.log(element)
+//     if (element <= 50) {return true;
+//     }
+    
+// })
+
+// console.log(newArr)
+
+
+// [20, 30, 40]
+// let newArr = arr.filter(element => element < 50) 
+
+// console.log(newArr)
+
+
+// let grades = [`A+`, `A`, `FAIL`]
+
+// // let newArr = arr.filter( element => element  = 'fail' )
+
+// // console.log(newArr)  
+
+// let goodGrades = grades.filter(element => element !== `FAIL` )
+
+// console.log(goodGrades)
+
+// let grades = [`FAIL`, `FAIL`, `B`,  ]
+
+// let goodGrades = grades.filter(element => element !== `FAIL` )
+
+// console.log(goodGrades);
+
+
+let grades = [`FAIL`,]
+
+let goodGrades = grades.filter(element => element !== `FAIL` )
+
+console.log(goodGrades);
